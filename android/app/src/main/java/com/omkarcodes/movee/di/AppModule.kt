b720d3181fde8acb926bb4aa7cac6c602878b9d8
Omkar <otbhostekar@gmail.com>
2022-05-29 00:50:29 +0530

    Android app changes

--- android/app/src/main/java/com/omkarcodes/movee/di/AppModule.kt
+++ android/app/src/main/java/com/omkarcodes/movee/di/AppModule.kt
@@ -17,6 +17,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -26,6 +27,7 @@ object AppModule {
     @Provides
     fun provideOkHttpClient(): OkHttpClient = OkHttpClient().newBuilder()
         .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .connectTimeout(2,TimeUnit.MINUTES)
         .build()
 
     @Provides
